9bbaa6db10d5c23c939b8ddad75cdf4a93da0015
LAB-2 <LAB-2@LAB2-36>
2023-08-17 11:44:10 -0500

    iniciando projecto

---
+++ src/main/java/upeu/edu/pe/project_lp2_gp2/ProjectLp2Gp2Application.java
@@ -0,0 +1,13 @@
+package upeu.edu.pe.project_lp2_gp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectLp2Gp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectLp2Gp2Application.class, args);
+	}
+
+}
---
+++ src/test/java/upeu/edu/pe/project_lp2_gp2/ProjectLp2Gp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package upeu.edu.pe.project_lp2_gp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectLp2Gp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
